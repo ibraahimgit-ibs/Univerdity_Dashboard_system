a598d7ceedd416e89e1a20f4cd70a69493a78af2
ibraahimgit-ibs <ibraahimgit@gmail.com>
2026-01-08 07:19:36 +0300

    Updated dashboard feature

--- src/components/student/dashboard/Dashboard.jsx
+++ src/components/student/dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
     console.log(data)
 
     return (
-        <div className="max-w-[210vh] min-w-[53vh]">
+        <div className=" w-full max-w-7xl mx-auto">
             <div className="space-y-3">
                 <h1 className="text-2xl">Welcome back, John!</h1>
                 <p className="text-gray-500">Here's your academic overview for the current semester.</p>
